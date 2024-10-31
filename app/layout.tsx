1ab855b7ff754ef8657c52e6e5007dfc3346e162
QiuLin-hao <210554114@qq.com>
2024-10-31 17:49:56 +0800

    在布局中添加导航链接和元数据
    在根布局中添加了 `NavLinks` 组件和元数据，简化了仪表盘布局，并引入了新的模板组件。

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,17 @@
 import React from "react";
+import {Metadata} from "next";
+import {NavLinks} from "@/app/ui/nav-links";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  )
+export const metadata: Metadata = {
+    title: 'Next.js',
+}
+export default function RootLayout({children}: { children: React.ReactNode }) {
+    return (
+        <html lang="en">
+        <body>
+        <NavLinks/>
+        <main>{children}</main>
+        </body>
+        </html>
+    )
 }
