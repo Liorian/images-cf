d8eb52f1b4aa194136288eacb09f8d21482df343
QiuLin-hao <210554114@qq.com>
2024-11-01 17:41:30 +0800

    添加 Vercel Analytics 到项目中
    本次提交通过在 `app/layout.tsx` 中添加必要的导入和组件来集成 Vercel Analytics，并更新了 `package-lock.json` 以包含所需的依赖项。

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import {Metadata} from "next";
 import NavBar from "@/app/ui/nav-bar";
 import NavFooter from "@/app/ui/nav-footer";
 import '@/app/globals.css'
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
     title: 'Next.js',
@@ -14,6 +15,7 @@ export default function RootLayout({children}: { children: React.ReactNode }) {
         <NavBar/>
         <main className="min-h-screen">{children}</main>
         <NavFooter/>
+        <Analytics />
         </body>
         </html>
     )
